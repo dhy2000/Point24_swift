89bd26854416aac8e775f8c31cb7ebe8e280c644
Mr Apple <apple@mrs-imac.local>
2020-10-04 17:44:39 +0800

    test

---
+++ Point24/GameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GameViewController.swift
+//  Point24
+//
+//  Created by Mr. Apple on 10/4/20.
+//  Copyright © 2020 Mr. Apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GameViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+    }
+    
+    
+    
+}
--- Point24/ViewController.swift
+++ Point24/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var exitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playOnClick(sender: UIButton, forEvent event: UIEvent) {
+        print("Hello, Let\'s Start Game")
+    }
+    
+    @IBAction func exitOnClick(sender: UIButton, forEvent event: UIEvent) {
+        print("Bye~")
+        exit(0)
     }
 
 
