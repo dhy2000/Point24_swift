b97502fdadb313bc7e07747bede483a92504fa1a
Mr Apple <apple@mrs-imac.local>
2020-10-04 16:55:15 +0800

    Initial Commit

---
+++ Point24/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Point24
+//
+//  Created by Mr. Apple on 10/4/20.
+//  Copyright © 2020 Mr. Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
